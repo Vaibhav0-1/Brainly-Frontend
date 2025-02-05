0bf5ff1821bf368b0808edb9b8f3a65441004aa0
Vaibhav <vaibhav.sharma3100@gmail.com>
2025-02-05 23:14:04 +0530

    Sidebar

--- .history/src/components/ui/Sidebar_20250205231333.tsx
+++ .history/src/components/ui/Sidebar_20250205231333.tsx
@@ -5,7 +5,10 @@ import { SidebarItems } from "./SidebarItem";
 export function Sidebar(){
     return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0">
         <div className="pt-4">
+            <div className="flex">
             <SidebarItems text={"Twitter"} icon={<TwitterIcon/>}/>
+            </div>
+            
             <SidebarItems text={"Youtube"} icon={<YoutubeIcon/>}/>
             
         </div>
--- .history/src/components/ui/Sidebar_20250205231340.tsx
+++ .history/src/components/ui/Sidebar_20250205231340.tsx
@@ -5,6 +5,7 @@ import { SidebarItems } from "./SidebarItem";
 export function Sidebar(){
     return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0">
         <div className="pt-4">
+            <div className="flex">
             <SidebarItems text={"Twitter"} icon={<TwitterIcon/>}/>
             <SidebarItems text={"Youtube"} icon={<YoutubeIcon/>}/>
             
